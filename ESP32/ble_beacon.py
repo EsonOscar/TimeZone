e50222c2a4d206fd8a381b6303e991170882fd5b
Eson <oser0001@stud.kea.dk>
2025-05-20 11:12:37 +0200

    BLE Beacon opdateringer.

--- ESP32/ble_beacon.py
+++ ESP32/ble_beacon.py
@@ -1,26 +1,32 @@
-import ubluetooth as bt, struct, time
-from micropython import const
+import ubluetooth as bt
+from time import sleep
+
+"""
+UUIDS:
+e1d807c6-6dd4-4345-a744-9c5b492144c1 (THIS ONE)
+
+f5ed34bf-1091-4715-8886-52cb7c606411
+
+3843a27a-1e93-4a1b-a89e-b0e0cef97c50
+
+5bae6c80-d05f-44bd-b093-59b8d4d9aba5
+"""
+
+UUID_BE = bytes.fromhex('e1d807c66dd44345a7449c5b492144c1')
+UUID_LE = UUID_BE[::-1]
+MESSAGE = b"Tractor A"
 
 ble = bt.BLE()
 ble.active(True)
 
-EDDYSTONE_UUID = const(0xFEAA)
-FRAME_TYPE_UID = b'\x00'
-NAMESPACE      = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A'
-INSTANCE       = b'HELLO!'    # 6 bytes
-TX_POWER       = b'\xd7'      # -41 dBm
-
 def advertise():
     # 1) Flags AD structure
     flags = b'\x02\x01\x06'
-    svc = (
-      bytes([len(FRAME_TYPE_UID + NAMESPACE + INSTANCE + TX_POWER) + 3, 0x16])
-      + struct.pack("<H", EDDYSTONE_UUID)
-      + FRAME_TYPE_UID + TX_POWER + NAMESPACE + INSTANCE
-    )
-    ble.gap_advertise(100_000, flags + svc)
+    svc_uuid = bytes([len(UUID_LE) + 1, 0x07]) + UUID_LE
+    svc_data = bytes([len(UUID_LE) + len(MESSAGE) + 1, 0x21]) + UUID_LE + MESSAGE
+    ble.gap_advertise(100_000, flags + svc_uuid + svc_data)
 
 while True:
-    print(f"Advertising: {INSTANCE}")
+    print(f"Advertising: {MESSAGE}")
     advertise()
-    time.sleep(1)
+    sleep(1)
